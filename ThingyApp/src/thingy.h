88087562d608ccb47ae48f37ea732d521893ecd1
mrolland <mrolland@debian.bnl.gov>
2019-06-19 14:27:51 -0400

    Document constants

--- ThingyApp/src/thingy.h
+++ ThingyApp/src/thingy.h
@@ -1,10 +1,24 @@
+
+// Note: MAX_NODES **must** match SERVER_COUNT as defined in the Thingy firmware.
 #define MAX_NODES 10
 #define BRIDGE_ID MAX_NODES
 
+// Bluetooth UUIDs for interacting with bridge
 #define SEND_UUID "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define RECV_UUID "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
+// sensorIDs for PVs
+#define BATTERY_ID 0
+#define RSSI_ID 1
+#define TEMPERATURE_ID 2
+#define HUMIDITY_ID 3
+#define PRESSURE_ID 4
+#define ACCELX_ID 5
+#define ACCELY_ID 6
+#define ACCELZ_ID 7
+
 #define COMMAND_LENGTH 7
+// Indices for command payload
 #define COMMAND_ID_MSB 0
 #define COMMAND_ID_LSB 1
 #define COMMAND_SERVICE 2
@@ -13,20 +27,7 @@
 #define COMMAND_PARAMETER3 5
 #define COMMAND_PARAMETER4 6
 
-#define RESPONSE_ID_MSB 0
-#define RESPONSE_ID_LSB 1
-#define RESPONSE_OPCODE 2
-
-#define SENSOR_TEMPERATURE_VAL 3
-#define SENSOR_TEMPERATURE_REM 4
-#define SENSOR_HUMIDITY 5
-#define SENSOR_PRESSURE_B1 6
-//#define SENSOR_PRESSURE_B2 7
-//#define SENSOR_PRESSURE_B3 8
-//#define SENSOR_PRESSURE_B4 9
-
-#define MOTION_ACCELX 3
-
+// Values for supported commands
 #define SERVICE_AUTO_PROVISION 0x01
 #define SERVICE_SENSOR 0x02
 #define SERVICE_LED 0x03
@@ -36,30 +37,45 @@
 #define SERVICE_PROVISION 0x07
 #define SERVICE_MOTION 0x08
 
-#define OPCODE_RSSI 0x03
-#define OPCODE_SENSOR_READING 0x06
-#define OPCODE_MOTION_READING 0x09
-#define OPCODE_BATTERY_READING 0x10
-
+// Parameters for sensor service
 #define SENSOR_STOP 0x00
 #define SENSOR_1S 0x01
 #define SENSOR_2s  0x02
 #define SENSOR_5s 0x03
 #define SENSOR_10s 0x04
-
-#define BATTERY_TYPE 3
-#define BATTERY_DATA 4
-
-#define BATTERY_TYPE_READING 0
-
+// Parameters for LED service
 #define LED_CONSTANT 0x01
 
-#define BATTERY_ID 0
-#define TEMPERATURE_ID 1
-#define HUMIDITY_ID 2
-#define PRESSURE_ID 3
-#define ACCELX_ID 4
-#define ACCELY_ID 5
-#define ACCELZ_ID 6
+// Indices for response payload
+#define RESPONSE_ID_MSB 0
+#define RESPONSE_ID_LSB 1
+#define RESPONSE_OPCODE 2
 
+// Opcodes for responses
+#define OPCODE_RSSI_READING 0x03
+#define OPCODE_SENSOR_READING 0x06
+#define OPCODE_MOTION_READING 0x09
+#define OPCODE_BATTERY_READING 0x10
+
+// Indices for environment sensor response
+#define SENSOR_TEMPERATURE_VAL 3
+#define SENSOR_TEMPERATURE_REM 4
+#define SENSOR_HUMIDITY 5
+#define SENSOR_PRESSURE_B1 6
+//#define SENSOR_PRESSURE_B2 7
+//#define SENSOR_PRESSURE_B3 8
+//#define SENSOR_PRESSURE_B4 9
+
+// Indices for motion sensor response
+#define MOTION_ACCELX 3
+
+// Indices for battery sensor response
+#define BATTERY_TYPE 3
+#define BATTERY_DATA 4
+#define BATTERY_TYPE_READING 0
+
+// Indices for RSSI response
+#define RSSI_DATA 3
+
+// Pointer for mac address given by thingyConfig()
 char mac_address[100];
